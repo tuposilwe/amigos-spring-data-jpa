08c4f443eeceaf53ab85750cc0ef547a67829d42
tuposilwe <rudigermkondya@gmail.com>
2024-04-28 12:30:18 +0300

    Adding Java Faker

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,86 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+             Student maria = new Student(
+                     "Maria",
+                     "Jones",
+                     "maria.jones@amigoscode.edu",
+                     21
+             );
+
+            Student maria2 = new Student(
+                    "Maria",
+                    "Jones",
+                    "maria2.jones@amigoscode.edu",
+                    25
+            );
+
+            Student ahmed = new Student(
+                    "Ahmed",
+                    "Ali",
+                    "ahmed.ali@amigoscode.edu",
+                    18
+            );
+
+//            studentRepository.saveAll(List.of(maria2));
+
+             studentRepository
+                     .findStudentByEmail("ahmed.ali@amigoscode.edu")
+                     .ifPresentOrElse(System.out::println,
+                             () -> System.out.println("student with email ahmed.ali@amigoscode.edu not found"));
+             studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterOrEqualNative(
+                     "Maria",
+                     18
+             ).forEach(System.out::println);
+        };
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+     @Query("SELECT s FROM Student s WHERE s.email = ?1")
+     Optional<Student> findStudentByEmail(String email);
+     @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
+     List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThan(
+             String firstName,Integer age);
+
+     @Query(
+             value = "select * from student where first_name = :firstName and age >= :age",
+             nativeQuery = true)
+     List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterOrEqualNative(
+             @Param("firstName") String firstName,
+             @Param("age") Integer age);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
