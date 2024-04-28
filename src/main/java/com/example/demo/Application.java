d40b663c8e8ebcd087b8f7393441b8fd93fda9f6
tuposilwe <rudigermkondya@gmail.com>
2024-04-28 13:35:01 +0300

    paging and sorting

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
@@ -19,14 +21,26 @@ public class Application {
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
         return args -> {
 //           generateRandomStudents(studentRepository);
-            Sort sort = Sort.by("firstName").ascending().and(Sort.by("age").descending());
 
-            studentRepository
-                    .findAll(sort)
-                    .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
-            ;
+            PageRequest pageRequest = PageRequest.of(
+                    0,
+                    10,
+                    Sort.by("firstName")
+                            .ascending());
+
+            Page<Student> page = studentRepository.findAll(pageRequest);
+            System.out.println(page);
         };
     }
+
+    private void sorting(StudentRepository studentRepository){
+        Sort sort = Sort.by("firstName").ascending().and(Sort.by("age").descending());
+
+        studentRepository
+                .findAll(sort)
+                .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
+    }
+
     private void generateRandomStudents(StudentRepository studentRepository){
         Faker faker = new Faker();
         for (int i = 0; i <= 20; i++){
--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface StudentRepository extends JpaRepository<Student,Long> {
+public interface StudentRepository extends PagingAndSortingRepository<Student,Long> {
      @Query("SELECT s FROM Student s WHERE s.email = ?1")
      Optional<Student> findStudentByEmail(String email);
      @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
